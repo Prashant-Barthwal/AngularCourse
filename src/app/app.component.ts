4fca31bf17c4a84cc25fcfcbf999e192d5717d93
Prashant <prashantbarthwal1234@gmail.com>
2022-10-05 19:40:12 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+
+@Component(
+  {
+    selector:'app-root',
+    //template:`<p>Hello world!</p>`,
+    templateUrl:'./app.component.html',
+    styleUrls:['./app.component.css'],
+
+  }
+)
+export class AppComponent{
+
+
+
+}
